2b4ccdbd2d8538f807e68b034f052f6271f9d6ec
wojtek <wojtek@Notebook.(none)>
2013-05-26 19:18:01 +0200

    modified swsingle

--- source/smithWaterman/swsingle.cpp
+++ source/smithWaterman/swsingle.cpp
@@ -22,18 +22,17 @@ SwSingle::~SwSingle()// Destruktor
 }
 
 void SwSingle::printMatrix(){
-    for(unsigned int i=0; i<this->sequenceA.length(); i++){
+    for(unsigned int i=0; i<this->sequenceB.length(); i++){
         if(i==0)cout<<"  ";
-        cout<<this->sequenceA[i]<<" ";
+        cout<<this->sequenceB[i]<<" ";
     }
     cout<<endl;
 
     for(unsigned int i=0; i<this->matrix.size(); i++) {
-        cout<<sequenceB[i]<<" ";
+        cout<<sequenceA[i]<<" ";
         for(unsigned int j=0; j<this->matrix[i].size(); j++) {
             cout<< this->matrix[i][j]<<" ";
         }
-
         cout<<endl;
     }
 }
@@ -49,34 +48,33 @@ void SwSingle::initMatrix(){
     }*/
 }
 
-//TODO
+
 void SwSingle::fillSwScoreMatrix(){
     int score = 1;
     for(unsigned int i=0; i<this->sequenceA.length(); i++){
         vector<int> row; // current row
         for(unsigned int j=0; j<this->sequenceB.length(); j++){
-            if(this->sequenceA[i]==this->sequenceB[j]){
-                score=1;
-            }else{
-                score=0;
-            }
-         row.push_back(score);
+            score = scoreSwMatrixElement(this->sequenceA[i],this->sequenceB[j]);
+            row.push_back(score);
         }
         matrix.push_back(row); // Add the row to the main vector
     }
 }
 
 int SwSingle::scoreSwMatrixElement(char charA,char charB){
-  double result=0;
+  double score=0;
   if(charA==charB){
-      //result=1;
-    }
-  else{
-      /*if(result>0){
-        result=-1;
-      }*/
-    }
-  return result;
+      if(score>=2){
+          score+=1;
+      }else{
+          score=2;
+      }
+  }else{
+      if(score>0){
+          score-=1;
+      }
+  }
+  return score;
 }
 
 double SwSingle::find_array_max(double array[],int length){
