b6f1ccf6fbbd4f6173de85c93924e73b3da23a65
Laurence Bortfeld <l.bortfeld@gmail.com>
2013-05-27 00:00:54 +0200

    Add samle extension

--- dna_alignment_dart_extension/smith_waterman_extension.dart
+++
@@ -1,7 +0,0 @@
-library smith_waterman;
-
-import 'dart_ext:smith_waterman_algorithm';
-
-// The simplest way to call native code: top-level functions
-int systemRand() native "SystemRand";
-bool sysemSrand(int seed) native "SystemSrand";
---
+++ dna_alignment_dart_server/sample_extension/sample_asynchronous_extension.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library sample_asynchronous_extension;
+
+import 'dart:async';
+import 'dart:isolate';
+import 'dart-ext:sample_extension';
+
+// A class caches the native port used to call an asynchronous extension.
+class RandomArray {
+  static SendPort _port;
+
+  Future<List<int> > randomArray(int seed, int length) {
+    var args = new List(2);
+    args[0] = seed;
+    args[1] = length;
+    return _servicePort.call(args).then((result) {
+      if (result != null) {
+        return result;
+      } else {
+        throw new Exception("Random array creation failed");
+      }
+    });
+  }
+
+  SendPort get _servicePort {
+    if (_port == null) {
+      _port = _newServicePort();
+    }
+    return _port;
+  }
+
+  SendPort _newServicePort() native "RandomArray_ServicePort";
+}
