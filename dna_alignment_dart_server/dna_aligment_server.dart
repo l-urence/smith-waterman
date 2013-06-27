1d7df8141491bf35762b7c3bb77c8d300d2685b9
Laurence Bortfeld <l.bortfeld@gmail.com>
2013-06-27 16:43:46 +0200

    Remove native dar extension. To complicated to build the extension using opencl.

--- dna_alignment_dart_server/dna_aligment_server.dart
+++
@@ -1,9 +0,0 @@
-library dna_aligment_server;
-
-import 'smith_waterman_extension/smith_waterman.dart';
-
-void main() {
-  SmithWaterman sw = new SmithWaterman();
-  sw.sayHello('Peter').then((e) => print(e));
-  // sw.sayHello('Peter').then((List e) => e.forEach((i) => print(i)));
-}
--- dna_alignment_dart_ui/web/dna_alignment_ui.dart
+++ dna_alignment_dart_ui/web/dna_alignment_ui.dart
@@ -3,6 +3,8 @@ import 'package:web_ui/web_ui.dart';
 
 // initial value for click-counter
 int startingCount = 5;
+const SERVER_URL = 'ws://127.0.0.1:4711';
+
 
 /**
  * Learn about the Web UI package by visiting
@@ -11,4 +13,19 @@ int startingCount = 5;
 void main() {
   // Enable this to use Shadow DOM in the browser.
   //useShadowDom = true;
+  connectToServer(SERVER_URL);
+
+}
+
+void connectToServer(url) {
+  final ws = new WebSocket(url);
+
+  ws.onOpen.listen((Event e) {
+    ws.send("Test");
+  });
+  ws.onMessage.listen((MessageEvent e) { 
+    print(e.data);
+    ws.close();
+    });
+  ws.onError.listen((Event err) => print(err));
 }
